112f0ba9630914815673a31da6f06c0bdca0988f
Lewon Güler <lewongueler@Dilaras-Mac.localdomain>
2019-10-11 10:31:57 +0200

    adding navigation

--- app/src/main/java/com/example/pokemonfinish/MainActivity.kt
+++ app/src/main/java/com/example/pokemonfinish/MainActivity.kt
@@ -2,11 +2,34 @@ package com.example.pokemonfinish
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.navigation.Navigation
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
+import com.example.pokemonfinish.databinding.ActivityMainBinding
+import de.ffuf.android.architecture.ui.base.binding.activities.BindingMvrxActivity
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BindingMvrxActivity<ActivityMainBinding>() {
+    override fun invalidate() {
+
+    }
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
+    private val navController by lazy {
+        Navigation.findNavController(findViewById<View>(R.id.myNavHostFragment))
+    }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        appBarConfiguration = AppBarConfiguration.Builder(navController.graph).build()
+
+        NavigationUI.setupActionBarWithNavController(this, navController)
+    }
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
